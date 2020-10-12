d1ac816c8af784c6827dc344b780ae73b0b6d92d
ersin <ersinclair@gmail.com>
2020-10-12 10:35:31 -0700

    Add project files.

---
+++ Lecture6Lab2/Program.cs
@@ -0,0 +1,113 @@
+﻿/// Lab No. 6		Exercise Part 2
+/// File Name: Fraction
+/// @author: Evan Sinclair
+/// Date:  Oct 6, 2020
+///
+/// Problem Statement: Define a class called Fraction. Include setter methods that allow the user to set the numerator
+/// and the denominator.  Include a method that returns the value of the numerator divided by the denominator as a 
+/// double.  Include an additional method that outputs the value of the fraction reduced to lowest terms (ie instead
+/// of outputting 20/60 output 1/3). Embed your class in a test program.
+/// 
+/// 
+/// Overall Plan:.
+/// 1)Build Class Fraction with numerator and denominator
+/// 2)Write a constructor and setters and getters
+/// 3)Write method that will return a reduced fraction
+/// 4)Build driver program to test Fraciton class
+
+
+using System;
+
+namespace Lecture6Lab2
+{
+    public class Fraction
+    {
+        
+        //Variables
+        private int numerator; 
+        private int denominator;
+
+        //Constructor
+        public Fraction(int numer, int denom)
+        {
+            SetNumerator(numer);
+            SetDenominator(denom);
+        }
+
+        //Setters
+        public void SetNumerator(int numer)
+        {
+            numerator = numer;
+        }
+        
+        // do not allow 0 for denominator
+        public void SetDenominator(int denom)
+        {
+            if (denom != 0)
+            {
+                denominator = denom;
+            }
+            else
+            {
+                Console.WriteLine("Error, cannot set denominator to 0. Setting denominator to 1");
+                SetDenominator(1);
+            }
+        }
+
+        //Getters
+
+        public int GetDenom()
+        {
+            return denominator;
+        }
+
+        public int GetNumer()
+        {
+            return numerator;
+        }
+
+        public double GetDouble()
+        {
+            return Convert.ToDouble(numerator) / denominator;
+        }
+        // Reduce fraction by finding GCD with the Euclidean algorithm and then
+        // dividing numerator and denominator by it
+
+        public void DisplayReducedFraction()
+        {
+            int a = denominator;
+            int b = numerator;
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            Console.WriteLine((numerator / a) + " / " + (denominator / a));
+        }
+
+
+
+    }
+
+    //Test Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a numerator and a denominator");
+            Fraction tester = new Fraction(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+
+            tester.DisplayReducedFraction();
+            Console.WriteLine(tester.GetDouble());
+
+        }
+
+
+    }
+
+    
+}
---
+++ Part1/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿/// Lab No. 6		Exercise Part 1
+/// File Name: Calculator
+/// @author: Evan Sinclair
+/// Date:  Oct 6, 2020
+///
+/// Problem Statement:Create a simple GUI that accepts a left hand value and a right hand value. Use a ListBox to
+/// have the user choose (+, -, *, /, %). Use try-catch blocks to handle bad input.  Display a MessageBox notifying
+/// the user of the error.  Have a button that calculates the results.
+/// 
+/// 
+/// Overall Plan:.
+/// 1)Create 3 Textboxes for 2 inputs and an output
+/// 2)Create a ListBox and populate with operators
+/// 3)Create a button to execute
+/// 4)Create a try catch block within the button to catch DivideByZeroException
+/// 5)create a switch statement to choose which operator will be used on the 2 input text boxes
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                int operation = OperationListBox.SelectedIndex;
+                decimal left = Convert.ToDecimal(LeftHandBox.Text);
+                decimal right = Convert.ToDecimal(RightHandBox.Text);
+                decimal result = 0;
+
+                switch (operation)
+                {
+                    case 0:
+                        {
+                            result = left + right;
+                            ResultTextBox.Text = result.ToString();
+                            break;
+                        }
+                    case 1:
+                        {
+                            result = left - right;
+                            ResultTextBox.Text = result.ToString();
+                            break;
+                        }
+                    case 2:
+                        {
+                            result = left * right;
+                            ResultTextBox.Text = result.ToString();
+                            break;
+                        }
+                    case 3:
+                        {
+                            result = left / right;
+                            ResultTextBox.Text = result.ToString();
+                            break;
+                        }
+                    case 4:
+                        {
+                            result = left % right;
+                            ResultTextBox.Text = result.ToString();
+                            break;
+                        }
+                    default:
+                        {
+                            result = 0;
+                            MessageBox.Show("No operator selected");
+                            break;
+                        }
+
+                }
+
+
+            }
+            catch(DivideByZeroException)
+            {
+                MessageBox.Show("Error: Cannot divide by 0");
+            }
+        }
+    }
+
+}
